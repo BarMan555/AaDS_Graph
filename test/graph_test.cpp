6916efba06097fe9c00c57e57e0fabaa0bd5e8fe
Борис Шестериков <boris20033@gmail.com>
2024-05-15 20:13:16 +0400

    Add tests

--- test/graph_test.cpp
+++ test/graph_test.cpp
@@ -1,94 +1,17 @@
 #include <gtest/gtest.h>
-#include "../include/set.h"
+#include "../include/graph.h"
 
-using namespace SetSpace;
+using namespace GraphSpace;
 
-TEST(set_test, insert)
+TEST(Graph, has_vertex)
 {
-	Set set;
-	set.insert(0);
-	
-	ASSERT_TRUE(set.contains(0));
+	Graph<int, int> graph;
+	ASSERT_FALSE(graph.has_vertex(10));
 }
 
-TEST(set_test, contains)
+TEST(Graph, add_vertex)
 {
-	Set set = { 0 };
-	bool in_set = set.contains(0);
-
-	ASSERT_TRUE(in_set);
-}
-
-TEST(set_test, erase)
-{
-	Set set = { 0, 1, 2 };
-	set.erase(0);
-	bool in_set = set.contains(0);
-
-	ASSERT_FALSE(in_set);
-}
-
-TEST(set_test, equel)
-{
-	Set set1 = { 0, 1, 2 };
-	Set set2 = { 0, 1, 2 };
-	ASSERT_TRUE(set1 == set2);
-}
-
-TEST(task_test, intersection_normal)
-{
-	Set set1 = {0, 10, 5, 25, 15};
-	Set set2 = {50, 5, 10, 20, 7};
-	
-	Set result_right = {5, 10};
-	Set result_intersection = intersection(set1, set2);
-	ASSERT_EQ(result_intersection, result_right);
-}
-
-TEST(task_test, intersection_empty)
-{
-	Set set1, set2;
-	Set result_right;
-	Set result_intersection = intersection(set1, set2);
-	ASSERT_EQ(result_intersection, result_right);
-}
-
-TEST(task_test, intersection_empty_one)
-{
-	Set set1, set2 = {5};
-	Set result_right;
-	Set result_intersection = intersection(set1, set2);
-	ASSERT_EQ(result_intersection, result_right);
-}
-
-TEST(task_test, difference_normal)
-{
-	Set set_d1 = {10, 1, 0, 12, 100};
-	Set set_d2 = {10, 2, 0, 15, 10};
-
-	Set result_right = {1, 12, 100};
-	Set result_difference = difference(set_d1, set_d2);
-	ASSERT_EQ(result_difference, result_right);
-}
-
-TEST(task_test, difference_empty_both)
-{
-	Set set_d1, set_d2, result_right;
-	Set result_difference = difference(set_d1, set_d2);
-	ASSERT_EQ(result_difference, result_right);
-}
-
-TEST(task_test, difference_empty_first)
-{
-	Set set_d1, set_d2 = {10, 12, 11}, result_right;
-	Set result_difference = difference(set_d1, set_d2);
-	ASSERT_EQ(result_difference, result_right);
-}
-
-TEST(task_test, difference_empty_second)
-{
-	Set set_d1 = {1, 2, 3}, set_d2;
-	Set result_right(set_d1);
-	Set result_difference = difference(set_d1, set_d2);
-	ASSERT_EQ(result_difference, result_right);
+	Graph<int, int> graph;
+	graph.add_vertex(10);
+	ASSERT_TRUE(graph.has_vertex(10));
 }
