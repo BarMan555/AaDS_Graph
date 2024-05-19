cbd0031025bdc8a6e22c93daf5f1e520cefacb76
Борис Шестериков <boris20033@gmail.com>
2024-05-19 10:18:02 +0400

    Add tests

--- test/graph_test.cpp
+++ test/graph_test.cpp
@@ -31,3 +31,30 @@ TEST(Graph, vertices_test)
 	graph.add_vertex(100);
 	ASSERT_EQ(graph.vertices(), std::vector<int>({10, 100}));
 }
+
+TEST(Graph, add_edge)
+{
+	Graph<int, int> graph;
+	graph.add_vertex(10);
+	graph.add_edge(10, 10, 1);
+	ASSERT_TRUE(graph.has_edge(10, 10));
+}
+
+TEST(Graph, has_edge_1)
+{
+	Graph<int, int> graph;
+	graph.add_vertex(10);
+	graph.add_vertex(15);
+	graph.add_edge(10, 15, 1);
+	ASSERT_TRUE(graph.has_edge(10, 15));
+}
+
+TEST(Graph, has_edge_2)
+{
+	Graph<int, int> graph;
+	graph.add_vertex(10);
+	graph.add_vertex(15);
+	graph.add_edge(10, 15, 1);
+	Graph<int, int>::Edge edge(10, 15, 1);
+	ASSERT_TRUE(graph.has_edge(edge));
+}
