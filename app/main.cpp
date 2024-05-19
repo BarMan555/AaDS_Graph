fa585e266143434d573db7b146ea1eeea9743250
Борис Шестериков <boris20033@gmail.com>
2024-05-19 14:29:32 +0400

    Write ex

--- app/main.cpp
+++ app/main.cpp
@@ -6,5 +6,14 @@ int main()
 	using namespace GraphSpace;
 	using namespace std;
 
+	Graph<int, int> gr;
+	gr.add_vertex(10);
+	gr.add_vertex(50);
+	gr.add_vertex(100);
+	gr.add_edge(10, 50, 1);
+	gr.add_edge(50, 100, 1);
+	gr.print(10);
+
+	gr.vector_walk(10);
 	return 0;
 }
